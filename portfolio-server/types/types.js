d3a11b09f52f28846383aa099c02d97d0cc5177c
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-03-18 14:43:04 -0500

    Updating project with types and current node modules.

--- portfolio-server/types/types.js
+++ portfolio-server/types/types.js
@@ -1,1 +1,31 @@
 const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+
+    type Query {
+        projects: [Projects!]!
+        technologies: [Technologies!]!
+    }
+
+    type Projects {
+        id: ID!
+        title: String!
+        description: String
+        image_url: String
+    }
+
+    type Technologies {
+        id: ID!
+        name: String!
+        description: String!
+    }
+
+    type Mutation {
+
+        newProject(id: ID!, title: String!, description: String!, image_url: String) : Projects
+        
+        newTechnology(id: ID!, name: String!, description: String!) : Technologies
+    }
+`
+
+module.exports = typeDefs
